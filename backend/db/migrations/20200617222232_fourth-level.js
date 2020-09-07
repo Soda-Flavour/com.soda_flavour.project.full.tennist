d89c1e02768a3294e8df74724a7198c3bcd9bc33
soda-flavour <axa8380@gmail.com>
2020-09-07 22:03:40 +0900

    refactor: Update db migration

--- backend/db/migrations/20200617222232_fourth-level.js
+++ backend/db/migrations/20200617222232_fourth-level.js
@@ -18,14 +18,20 @@ exports.up = async (knex) => {
   await Promise.all([
     knex.schema.createTable(tableNames.userRacketHistory, (table) => {
       table.increments('id').notNullable();
+      references(table, tableNames.user);
       references(table, tableNames.userRacket);
-      table.integer('weight_tune').unsigned();
-      references(table, tableNames.replacementGrip);
-      table.integer('overgrip_num').unsigned();
+      references(table, tableNames.replacementGrip, false);
       references(table, tableNames.racketbalance);
       references(table, tableNames.gut);
-      references(table, tableNames.gutTension, false, 'main_gut_tension_id');
-      references(table, tableNames.gutTension, false, 'cross_gut_tension_id');
+      references(table, tableNames.gutTension, false, 'cross_gut_tension');
+      references(table, tableNames.gutTension, false, 'main_gut_tension');
+      table.integer('weight_tune').unsigned();
+      table.string('replacement_grip_type').notNullable();
+      table.integer('overgrip_num').unsigned();
+      table.specificType('racket_balance_type', 'char(2)');
+      table.float('racket_balance_val', 4).unsigned();
+      table.float('main_gut_lb_tension', 4).unsigned();
+      table.float('cross_gut_lb_tension', 4).unsigned();
       addDefaultColumns(table);
     }),
   ]);
